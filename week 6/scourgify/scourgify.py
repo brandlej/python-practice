d2ea515736d96a233b4713aeb2fb155d583c1ff5
Brandon Jones <orthofid013@gmail.com>
2026-03-23 21:06:30 -0600

    improve solution

--- week 6/scourgify/scourgify.py
+++ week 6/scourgify/scourgify.py
@@ -11,29 +11,19 @@ def main():
 
     source_csv = sys.argv[1]
     target_csv = sys.argv[2]
-    source_data: list[dict[str, str]] = []
 
-    # could improve by writing to target dict as i'm reading
-
-    # read csv rows into dict
+    # python allows opening both files in one line with comma
+    # use streaming approach to write as i'm reading the source csv
     try:
-        with open(source_csv) as file:
-            reader = csv.DictReader(file)
+        with open(source_csv) as source, open(target_csv, "w") as target:
+            reader = csv.DictReader(source)
+            writer = csv.DictWriter(target, fieldnames=["first", "last", "house"])
+            writer.writeheader()
             for row in reader:
                 last, first = row["name"].split(", ")
-                source_data.append(
-                    {"first": first, "last": last, "house": row["house"]}
-                )
+                writer.writerow({"first": first, "last": last, "house": row["house"]})
     except FileNotFoundError:
         sys.exit(f"Could not read {source_csv}")
-
-    # write csv rows into dict
-    # with updated csv header first, last, house
-    with open(target_csv, "w") as file:
-        writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
-        writer.writeheader()
-        for entry in source_data:
-            writer.writerow(entry)
 
 
 if __name__ == "__main__":
